0c537d819164233991bb22064bee15db65122437
alphacolony <austenf22@gmail.com>
2026-08-04 12:16:51 -0700

    build: include parallel-class partition layer

--- Rank3KUM.lean
+++ Rank3KUM.lean
@@ -1,6 +1,7 @@
 import Rank3KUM.Basic
 import Rank3KUM.UniformDensity
 import Rank3KUM.HalfWeave.RankTwo
+import Rank3KUM.HalfWeave.ParallelClasses
 import Rank3KUM.TwoGap.Closure
 import Rank3KUM.TwoGap
 import Rank3KUM.TwoGap.Support
