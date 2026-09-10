0199c8e273bda3a025184e9ec58f6a264756f3ab
afletc-a11y <amfletc3@asu.edu>
2026-09-10 11:19:55 -0700

    v2: include structural six-point bounds

--- Rank3KUM.lean
+++ Rank3KUM.lean
@@ -38,3 +38,4 @@ import Rank3KUM.Version2.NearTightClassification
 import Rank3KUM.Version2.CorrespondenceWrappers
 import Rank3KUM.Version2.NearTightHitting
 import Rank3KUM.Version2.ResidualSupportDisjoint
+import Rank3KUM.Version2.SixPointStructural
