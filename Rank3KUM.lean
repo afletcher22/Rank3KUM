41eb318113f7f9266cc6693c0a5794a773286003
afletc-a11y <amfletc3@asu.edu>
2026-09-10 11:45:46 -0700

    v2: gate Pasch structural classification

--- Rank3KUM.lean
+++ Rank3KUM.lean
@@ -41,3 +41,4 @@ import Rank3KUM.Version2.ResidualSupportDisjoint
 import Rank3KUM.Version2.SixPointStructural
 import Rank3KUM.Version2.SixPointClassification
 import Rank3KUM.Version2.SixPointMaximal
+import Rank3KUM.Version2.SixPointPasch
