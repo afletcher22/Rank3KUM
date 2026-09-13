28a4c037220bf0789ef9fc94a71876020bdf73ca
afletc-a11y <amfletc3@asu.edu>
2026-09-13 11:40:13 -0700

    experiment: build generic cyclic gluing interface

--- Rank3KUM.lean
+++ Rank3KUM.lean
@@ -1,6 +1,7 @@
 import Rank3KUM.Basic
 import Rank3KUM.UniformDensity
 import Rank3KUM.TightContraction
+import Rank3KUM.GenericGluing
 import Rank3KUM.HalfWeave.RankTwo
 import Rank3KUM.HalfWeave.ParallelClasses
 import Rank3KUM.TwoGap.Closure
